c395345d43de862f78e8a8228dcec7a2586eb221
NickDubDrums <niko.debe.98.nd@gmail.com>
2025-06-23 15:23:11 +0200

    yo

--- editor.js
+++ editor.js
@@ -443,7 +443,7 @@ function save() {
     });
 
   
-console.log("Hidden Songs:", hiddenSongs);
+//console.log("Hidden Songs:", hiddenSongs);
 
 
 
@@ -541,9 +541,13 @@ function renderEditorTable() {
       const li = document.createElement("li");
       li.classList.add("hidden-song");
       li.innerHTML = `
-        <div class="svg-edit" style="display:flex;justify-content:space-between;align-items:center;">
+        <div class="hidden-songs-grid">
           <span style="opacity: 0.6; font-style: italic;">${song}</span>
-          <button class="btn btn-small show-btn" title="Rendi visibile">👁️</button>
+          <button class="btn btn-small show-btn" title="Rendi visibile"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+    stroke-width="1.5" stroke="currentColor" class="size-6">
+    <path stroke-linecap="round" stroke-linejoin="round"
+      d="M3.98 8.223A10.477 10.477 0 0 0 1.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.451 10.451 0 0 1 12 4.5c4.756 0 8.773 3.162 10.065 7.498a10.522 10.522 0 0 1-4.293 5.774M6.228 6.228 3 3m3.228 3.228 3.65 3.65m7.894 7.894L21 21m-3.228-3.228-3.65-3.65m0 0a3 3 0 1 0-4.243-4.243m4.242 4.242L9.88 9.88" />
+  </svg></button>
         </div>
       `;
       li.querySelector(".show-btn").onclick = () => {
@@ -685,7 +689,17 @@ function apriMenuModifica(index, branoCorrente, utenteCorrente) {
   toggleVisibilityBtn.onclick = null;
 
   const èNascosto = hiddenSongs.some(s => s.trim() === branoCorrente.trim());
-  toggleVisibilityBtn.textContent = èNascosto ? "Rendi visibile" : "Nascondi brano";
+ toggleVisibilityBtn.innerHTML = èNascosto
+  ? `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+  <path stroke-linecap="round" stroke-linejoin="round" d="M2.036 12.322a1.012 1.012 0 0 1 0-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178Z" />
+  <path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+</svg>
+ Rendi visibile`
+  : `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+  <path stroke-linecap="round" stroke-linejoin="round" d="M3.98 8.223A10.477 10.477 0 0 0 1.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.451 10.451 0 0 1 12 4.5c4.756 0 8.773 3.162 10.065 7.498a10.522 10.522 0 0 1-4.293 5.774M6.228 6.228 3 3m3.228 3.228 3.65 3.65m7.894 7.894L21 21m-3.228-3.228-3.65-3.65m0 0a3 3 0 1 0-4.243-4.243m4.242 4.242L9.88 9.88" />
+</svg>
+ Nascondi brano`;
+
   toggleVisibilityBtn.style.display = "inline-block"; // Assicurati sia visibile
 
   toggleVisibilityBtn.onclick = async () => {
@@ -707,7 +721,14 @@ function apriMenuModifica(index, branoCorrente, utenteCorrente) {
   ]);
 
   // Aggiorna UI
-  toggleVisibilityBtn.textContent = èNascosto ? "Nascondi brano" : "Rendi visibile";
+  toggleVisibilityBtn.innerHTML = èNascosto ? `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+  <path stroke-linecap="round" stroke-linejoin="round" d="M3.98 8.223A10.477 10.477 0 0 0 1.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.451 10.451 0 0 1 12 4.5c4.756 0 8.773 3.162 10.065 7.498a10.522 10.522 0 0 1-4.293 5.774M6.228 6.228 3 3m3.228 3.228 3.65 3.65m7.894 7.894L21 21m-3.228-3.228-3.65-3.65m0 0a3 3 0 1 0-4.243-4.243m4.242 4.242L9.88 9.88" />
+</svg>
+ Nascondi brano` : `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+  <path stroke-linecap="round" stroke-linejoin="round" d="M2.036 12.322a1.012 1.012 0 0 1 0-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178Z" />
+  <path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+</svg>
+ Rendi visibile`;
   renderEditorList();
 };
 
